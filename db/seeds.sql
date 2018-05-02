72ee6cd1a0a7a9e25deae6e9bfd8ea47bc707fe1
Gurnam Sunny <Sunny@Sunnys.local>
2018-05-02 09:29:07 -0700

    final changes

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
